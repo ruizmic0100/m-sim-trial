a2f3ac15ea0f7e5f74671adcffd0c2fb712a3a54
Michael <ruizmic0100@hotmail.com>
2023-01-24 00:24:24 -0600

    Fixed the issue with the graph not showing the right plot. Turns out it was just a versioning issue. Updated build-tools a bit.

--- build-tools/data-visualization.py
+++ build-tools/data-visualization.py
@@ -1,33 +1,71 @@
 # Finding throughput IPC and saving that result and then displaying it on a graph.
 import matplotlib.pyplot as plt
 
-total_sim_runs_path = f'./simulation-results/total_sim_runs.txt'
-reg_cap = [8, 9, 10, 32]
-throughput_ipc = [None] * len(reg_cap)
+total_sim_runs_path = './simulation-results/total_sim_runs.txt'
+reg_cap = [i for i in range(1,33)]
+throughput_ipc_1 = [None] * 32
+throughput_ipc_2 = [None] * 32
+throughput_ipc_3 = [None] * 32
+throughput_ipc_4 = [None] * 32
 
 def get_throughput_ipc(total_runs):
-    i = 0
-    while i < total_runs:
-        with open(f'./simulation-results/run_{i+1}.txt', 'r') as file:
-            content = file.read()
-            subtr_target = content.find("THROUGHPUT IPC: ")
-            throughput_ipc[i] = float(content[subtr_target+16:subtr_target+16+8])
-            i += 1
-        file.close()
+    if total_runs == 32:
+        for i in range(0,32):
+            with open(f'./simulation-results/run_{i+1}.txt', 'r') as file:
+                content = file.read()
+                subtr_target = content.find("THROUGHPUT IPC: ")
+                throughput_ipc_1[i] = float(content[subtr_target+16:subtr_target+16+8])
+                file.close()
+    if total_runs == 64:
+        for i in range(0,32):
+            with open(f'./simulation-results/run_{i+1}.txt', 'r') as file:
+                content = file.read()
+                subtr_target = content.find("THROUGHPUT IPC: ")
+                throughput_ipc_2[i] = float(content[subtr_target+16:subtr_target+16+8])
+                file.close()
+    if total_runs == 96:
+        for i in range(0,32):
+            with open(f'./simulation-results/run_{i+1}.txt', 'r') as file:
+                content = file.read()
+                subtr_target = content.find("THROUGHPUT IPC: ")
+                throughput_ipc_3[i] = float(content[subtr_target+16:subtr_target+16+8])
+                file.close()
+    if total_runs == 128:
+        for i in range(0,32):
+            with open(f'./simulation-results/run_{i+1}.txt', 'r') as file:
+                content = file.read()
+                subtr_target = content.find("THROUGHPUT IPC: ")
+                throughput_ipc_4[i] = float(content[subtr_target+16:subtr_target+16+8])
+                file.close()
+
+
 
 with open(total_sim_runs_path, 'r') as total_runs_file:
     data = total_runs_file.read()
-    total_runs = data[0]
-    get_throughput_ipc(int(total_runs))
-
-    print(throughput_ipc)
-
-
-    plt.plot(reg_cap, throughput_ipc)
-    plt.title("Register Capping vs. IPC Throughput")
-    plt.ylabel("IPC Throughput")
-    plt.xlabel("Register Cap Value")
+    total_runs = [ int(x) for x in data.split() ]
+    get_throughput_ipc(total_runs[0])
+    print(throughput_ipc_1)
+    print("\n\n")
+    print(throughput_ipc_2)
 
 
+    print(max(throughput_ipc_1))
+    percentage_1 = ((max(throughput_ipc_1) - throughput_ipc_1[31]) / throughput_ipc_1[31])
+    print(percentage_1 * 100)
+
+    if total_runs[0] == 32:
+        plt.figure(1)
+        plt.plot(reg_cap, throughput_ipc_1)
+        plt.title("Register Capping vs. IPC Throughput")
+        plt.ylabel("IPC Throughput")
+        plt.xlabel("Register Cap Value")
+
+    if total_runs[0] == 64:
+        plt.figure(2)
+        plt.plot(reg_cap, throughput_ipc_2)
+        plt.title("Register Capping vs. IPC Throughput_2")
+        plt.ylabel("IPC Throughput")
+        plt.xlabel("Register Cap Value")
+        
+        
     plt.show()
-
--- build-tools/run.py
+++ build-tools/run.py
@@ -1,5 +1,45 @@
 import os
 import subprocess
+import sys
+import argparse
+
+
+def run_standard(int_reg_cap):
+    command = f"cd ../spec2006/ && ../m-sim-trial/m-sim/sim-outorder -rf:size 160 -max_contexts_per_core 4 -int_reg_cap {int_reg_cap} astarNS.1.arg dealIINS.1.arg gobmkNS.1.arg lbmNS.1.arg"
+    subprocess.run(command, shell=True);
+
+def run_mix(int_reg_cap):
+    for run in range(1, 33):
+        command = f"cd ../spec2006/ && ../m-sim-trial/m-sim/sim-outorder -rf:size 160 -max_contexts_per_core 4 -int_reg_cap {run} astarNS.1.arg dealIINS.1.arg gobmkNS.1.arg lbmNS.1.arg"
+        subprocess.run(command, shell=True);
+    for run in range(1, 33):
+        command = f"cd ../spec2006/ && ../m-sim-trial/m-sim/sim-outorder -rf:size 160 -max_contexts_per_core 4 -int_reg_cap {run} astarNS.1.arg gccNS.1.arg lbmNS.1.arg namdNS.1.arg"
+        subprocess.run(command, shell=True);
+    for run in range(1, 33):
+        command = f"cd ../spec2006/ && ../m-sim-trial/m-sim/sim-outorder -rf:size 160 -max_contexts_per_core 4 -int_reg_cap {run} astarNS.1.arg gobmkNS.1.arg milcNS.1.arg sjengNS.1.arg"
+        subprocess.run(command, shell=True);
+    for run in range(1, 33):
+        command = f"cd ../spec2006/ && ../m-sim-trial/m-sim/sim-outorder -rf:size 160 -max_contexts_per_core 4 -int_reg_cap {run} bzip2NS.1.arg dealIINS.1.arg gccNS.1.arg gobmkNS.1.arg"
+        subprocess.run(command, shell=True);
+
+def run_basic_mix(int_reg_cap):
+    for run in range(1, 33):
+        command = f"cd ../spec2006/ && ../m-sim-trial/m-sim/sim-outorder -rf:size 160 -max_contexts_per_core 4 -int_reg_cap {run} astarNS.1.arg dealIINS.1.arg gobmkNS.1.arg lbmNS.1.arg"
+        subprocess.run(command, shell=True);
+
+FUNCTION_MAP = {'run_standard' : run_standard,
+                'run_shmix' : run_mix,
+                'run_basic_shmix' : run_basic_mix }
+
+parser = argparse.ArgumentParser(description='run m-sim')
+
+parser.add_argument('command', choices=FUNCTION_MAP.keys(),
+                    help='run a single four thread mix of different benchmarks.')
+
+parser.add_argument('int_reg_cap', nargs='?', type=int)
+
+args = parser.parse_args()
 
 if __name__ == "__main__":
-    subprocess.run("cd ../spec2006/ && ../m-sim-trial/m-sim_v3.0/sim-outorder -rf:size 160 -max_contexts_per_core 4 -int_reg_cap 32 astarNS.1.arg dealIINS.1.arg gobmkNS.1.arg lbmNS.1.arg", shell=True);
+    func = FUNCTION_MAP[args.command]
+    func(args.int_reg_cap)
